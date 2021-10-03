3a092ad268ee52b89f0440aa57200444d8557b1d
Mario <mariosr@ciandt.com>
2021-10-03 18:35:50 -0300

    Included new rules

--- background.js
+++ background.js
@@ -28,29 +28,98 @@ function ticketValidator() {
         },
 
         validateFieldById: function () {
-            var fields = ['versions-val', 'fixfor-val', 'priority-val', 'components-val', 'customfield_11300-val', 'customfield_11100-val', 'customfield_10004-val'];
+            var wrongFields = false, missingFields = [];
+            var fields = {
+                'versions-val': "Affects Version/s", 
+                'fixfor-val': "Fix Version/s", 
+                'priority-val': "Priority", 
+                'components-val': "Component/s", 
+                'customfield_11300-val': "Severity", 
+                'customfield_11100-val': "Environment Affected", 
+                'customfield_10004-val': "Sprint"
+            };
 
             if (this.isStatusCompleted()) {
-                fields.push('resolution-val', 'customfield_10200-val', 'customfield_10106-val');
+                fields['resolution-val'] = "Resolution";
+                fields['customfield_10200-val'] = "Issue Resolution";
+                fields['customfield_10106-val'] = "Root Cause";
             }
 
-            fields.forEach(element => {
+            for (var element in fields) {
                 var el = document.getElementById(element)
                 if (el !== null && el !== undefined) {
                     var trimmedElValue = el.innerText.trim();
                     var statusCorrect = !['None', 'Unresolved'].includes(trimmedElValue);
-                    this.setStatusMark(statusCorrect, el)
+                    this.setStatusMark(statusCorrect, el);
+                    wrongFields = wrongFields || !statusCorrect;
+                    continue;
                 }
-            });
+                missingFields.push(fields[element]);
+            }
+
+            this.setStatusReport(missingFields, wrongFields);
+        },
+
+        setStatusReport: function (missingFields) {
+            var reportElement = document.querySelector('.jira-ticket-validator');
+
+            if (missingFields.length == 0) {
+                if (reportElement) {
+                    reportElement.parentNode.removeChild(reportElement);
+                }
+                return;
+            }
+
+            if (!reportElement) {
+                reportElement = document.createElement('div')
+            }
+
+            reportElement.className = 'jira-ticket-validator status-report';
+            reportElement.innerHTML = '';
+            reportElement.className += ' errors';
+            reportElement.innerHTML = `<span><strong>The following fields are missing:</strong> ${missingFields.join(', ')}</span>`;    
+
+            if (!this.validateWorkLog()) {
+                reportElement.innerHTML += '<span><strong>There are no work logged in this task</strong></span>';
+            }
+
+            var bodyContentElement = document.querySelector('.issue-body-content');
+            bodyContentElement.insertBefore(reportElement, bodyContentElement.firstChild);
+        },
+
+        validateDescriptionSteps: function () {
+            if (!this.isStatusCompleted()) {
+                return;
+            }
+            var descriptionLabel = document.getElementById('descriptionmodule_heading').querySelector('h4');
+            var descriptionField = document.getElementById('description-val').innerText.toLowerCase();
+            var cond1 = descriptionField.includes('steps to reproduce');
+            var cond2 = descriptionField.includes('actual result');
+            var cond3 = descriptionField.includes('expected result');
+            var validField = cond1 && cond2 && cond3;
+            this.setStatusMark(validField, descriptionLabel);
         },
 
         isStatusCompleted: function () {
             var statusComponent = document.getElementById('status-val').querySelector('span');
-            return statusComponent.innerHTML === 'Completed';
+            return ['completed', 'in review'].includes(statusComponent.innerText.toLowerCase());
+        },
+
+        validateWorkLog: function () {
+            if (!this.isStatusCompleted()) {
+                console.log('test')
+                return true;
+            }
+
+            var element = document.getElementById('issue_actions_container');
+            var logsEmpty = element.innerText.trim().toLowerCase().includes('no work has yet been logged on this issue.');
+
+            return !logsEmpty;
         },
     }
     funcs.validateFieldById();
     funcs.validateFieldLabels();
+    funcs.validateDescriptionSteps();
 }
 
 chrome.action.onClicked.addListener((tab) => {
